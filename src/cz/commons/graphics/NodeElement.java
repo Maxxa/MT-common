554704478c33ad045737de41e56e18eeef48b80b
Viktor Krejčíř <krejcirviktor@gmail.com>
2014-11-15 21:03:52 +0100

    NodeElement edited
    Refactoring

--- src/cz/commons/graphics/NodeElement.java
+++ src/cz/commons/graphics/NodeElement.java
@@ -5,85 +5,112 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
 /**
- * Zakladni trida pro prvky s jednim propojnym bodem.
+ * Basic class for elements with <b>single</b> connection point
+ * 
  * @author Martin Šára
+ * @author Viktor Krejčíř (refactoring)
  */
 public abstract class NodeElement extends Element implements ConnectibleElement {
-        
-    private final DoubleProperty centerX = new SimpleDoubleProperty();
-    private final DoubleProperty centerY = new SimpleDoubleProperty();
 
-    /**
-     * Vytvoreni elementu na absolutnich souradnicich.
-     * @param x
-     * @param y
-     * @param width
-     * @param height
-     * @param id 
-     */
-    protected NodeElement(int x, int y, int width, int height, int id) {
-        this.id = id;
-        setTranslateX(x);
-        setTranslateY(y);
-        doBindings(width, height);
-    }
-    
-    /**
-     * Vytvoreni elementu relativne posunuteho od vztazneho elementu.
-     * @param anchor
-     * @param x
-     * @param y
-     * @param width
-     * @param height
-     * @param id 
-     */
-    protected NodeElement(NodeElement anchor, int x, int y, int width, int height, int id) {
-        this.id = id;
-        setTranslateX(x + anchor.getTranslateX());
-        setTranslateY(y + anchor.getTranslateY());
-        setAnchorNode(anchor);
-        doBindings(width, height);
-    }
+	private final DoubleProperty centerX = new SimpleDoubleProperty();
+	private final DoubleProperty centerY = new SimpleDoubleProperty();
 
-    /**
-     * Navazani vlastnosti centerX/Y na aktualni posuti.
-     * @param width
-     * @param height 
-     */
-    private void doBindings(int width, int height) {
-        centerX.bind(Bindings.add(width/2, translateXProperty()));
-        centerY.bind(Bindings.add(height/2, translateYProperty()));
-    }
-        
-    @Override
-    public DoubleProperty connectXProperty() {
-        return centerX;
-    }
+	/**
+	 * Create new element on specified location
+	 * 
+	 * @param x
+	 *            location of element
+	 * @param y
+	 *            location of element
+	 * @param width
+	 *            of element
+	 * @param height
+	 *            of element
+	 * @param id
+	 *            - unique id of element
+	 */
+	protected NodeElement(int x, int y, int width, int height, int id) {
+		this.id = id;
+		setTranslateX(x);
+		setTranslateY(y);
+		doBindings(width, height);
+	}
 
-    @Override
-    public DoubleProperty connectYProperty() {
-        return centerY;
-    }
-    
-    /**
-     * Navazani pozice na vztazny element.
-     * @param anchor vztazny element
-     */
-    protected final void setAnchorNode(NodeElement anchor) {       
-        // zachovani celych cisel (animace pracuji s realnymi cisly)
-        long dx = Math.round(getTranslateX() - anchor.getTranslateX());
-        long dy = Math.round(getTranslateY() - anchor.getTranslateY());
-        
-        translateXProperty().bind(Bindings.add(dx, anchor.translateXProperty()));
-        translateYProperty().bind(Bindings.add(dy, anchor.translateYProperty()));
-    }
-    
-    /**
-     * Odpoutani od vztazneho elementu.
-     */
-    protected void removeAnchorNode() {        
-        translateXProperty().unbind();
-        translateYProperty().unbind();
-    }
-    
+	/**
+	 * Create new element moved by x,y from anchor element.
+	 * 
+	 * @param anchor
+	 *            Anchor element used moved from.
+	 * @param x
+	 *            moved by on x axis.
+	 * @param y
+	 *            moved by on y axis
+	 * @param width
+	 *            of element
+	 * @param height
+	 *            of element
+	 * @param id
+	 *            - unique id of element
+	 */
+	protected NodeElement(NodeElement anchor, int x, int y, int width,
+			int height, int id) {
+		this.id = id;
+		setTranslateX(x + anchor.getTranslateX());
+		setTranslateY(y + anchor.getTranslateY());
+		setAnchorNode(anchor);
+		doBindings(width, height);
+	}
+
+	/**
+	 * Binds center properties to translate properties. Therefore - when
+	 * translate property changes (via animation), the centerX and Y changes
+	 * too.
+	 * 
+	 * @param width
+	 *            of element
+	 * @param height
+	 *            of element
+	 */
+	private void doBindings(int width, int height) {
+		centerX.bind(Bindings.add(width / 2, translateXProperty()));
+		centerY.bind(Bindings.add(height / 2, translateYProperty()));
+	}
+
+	@Override
+	public DoubleProperty connectXProperty() {
+		return centerX;
+	}
+
+	@Override
+	public DoubleProperty connectYProperty() {
+		return centerY;
+	}
+
+	/**
+	 * Binding position to anchor element. Therefore when anchor position
+	 * changes, position of this element changes too
+	 * 
+	 * @param anchor
+	 *            anchor element
+	 */
+	protected final void setAnchorNode(NodeElement anchor) {
+		// zachovani celych cisel (animace pracuji s realnymi cisly)
+		long dx = Math.round(getTranslateX() - anchor.getTranslateX());
+		long dy = Math.round(getTranslateY() - anchor.getTranslateY());
+
+		translateXProperty()
+				.bind(Bindings.add(dx, anchor.translateXProperty()));
+		translateYProperty()
+				.bind(Bindings.add(dy, anchor.translateYProperty()));
+	}
+
+	/**
+	 * Unbinds from anchor.This element makes it no longer dependent on anchor's
+	 * position.
+	 */
+	protected void removeAnchorNode() {
+		translateXProperty().unbind();
+		translateYProperty().unbind();
+	}
+
 }
