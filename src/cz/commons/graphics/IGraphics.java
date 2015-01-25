b5eb28864c07ff4bc85f4c4613dc2604e156f4cf
Vojtěch Müller <muller.voj@gmail.com>
2015-01-25 10:13:47 +0100

    Add constant for DPI recalculating.

---
+++ src/cz/commons/graphics/IGraphics.java
@@ -0,0 +1,18 @@
+package cz.commons.graphics;
+
+import javafx.scene.text.Font;
+
+/**
+ * @author Vojtěch Müller
+ */
+public class IGraphics {
+
+    /**
+     * Vychozi velikost fontu v systemu respektujici aktualni DPI.
+     */
+    public static final double DEFAULT_FONT_SIZE = Font.getDefault().getSize();
+    /**
+     * Konstanta udavajici zvetseni oproti standardnimu DPI.
+     */
+    public static final double PLATFORM_SCALE = DEFAULT_FONT_SIZE / 12.0;
+}
