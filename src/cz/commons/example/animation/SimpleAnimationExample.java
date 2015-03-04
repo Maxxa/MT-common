f5333cd8a72d6c09940b8a87647bc5905e712c14
Vojtěch Müller <muller.voj@gmail.com>
2015-03-04 21:14:10 +0100

    Update example with animation with Timeline

--- src/cz/commons/example/animation/SimpleAnimationExample.java
+++ src/cz/commons/example/animation/SimpleAnimationExample.java
@@ -15,42 +15,43 @@ import cz.commons.example.graphics.branchNode.ExampleBranchNode;
 
 public class SimpleAnimationExample extends AbstractExample {
 
-	private Button goForward = new Button("Forward");
+    private Button goForward = new Button("Forward");
 
-	private Button goBack = new Button("Back");
-	private ExampleBranchNode node = new ExampleBranchNode(0, 0, 50, 2);
+    private Button goBack = new Button("Back");
+    private ExampleBranchNode node = new ExampleBranchNode(0, 0, 50, 2);
 
     private final Boolean IS_STEPPING = true;
 
-	private AnimationControl ac = new AnimationControl();
-	public SimpleAnimationExample() {
-		super();
-		initHandlers();
-		createAnimation();
+    private AnimationControl ac = new AnimationControl();
 
-	}
+    public SimpleAnimationExample() {
+        super();
+        initHandlers();
+        createAnimation();
 
-	private void initHandlers() {
-		ac.markAsStepping(IS_STEPPING);
+    }
 
-		goForward.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent event) {
-				ac.playForward();
+    private void initHandlers() {
+        ac.markAsStepping(IS_STEPPING);
+
+        goForward.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                ac.playForward();
                 controlIsNext();
 
-			}
-		});
-		goBack.setOnAction(new EventHandler<ActionEvent>() {
+            }
+        });
+        goBack.setOnAction(new EventHandler<ActionEvent>() {
 
-			@Override
-			public void handle(ActionEvent event) {
-				ac.goBack();
+            @Override
+            public void handle(ActionEvent event) {
+                ac.goBack();
                 controlIsNext();
 
-			}
-		});
+            }
+        });
 
         ac.addTransitionFinishedListener(new TransitionFinishedEvent() {
             @Override
@@ -58,46 +59,57 @@ public class SimpleAnimationExample extends AbstractExample {
                 System.out.println(type);
             }
         });
-	}
-
-    private void controlIsNext(){
-        System.out.println("Is previous: "+ ac.isPreviousTransition());
-        System.out.println("Is next: "+ ac.isNextTransition());
     }
 
-	@Override
-	protected void initPane(Pane canvas) {
-		HBox menu = new HBox();
-		menu.getChildren().addAll(goBack, goForward);
-		canvas.getChildren().add(menu);
-		// node.setTranslateY(50);
-		canvas.getChildren().add(node);
+    private void controlIsNext() {
+        System.out.println("Is previous: " + ac.isPreviousTransition());
+        System.out.println("Is next: " + ac.isNextTransition());
+    }
 
-	}
+    @Override
+    protected void initPane(Pane canvas) {
+        HBox menu = new HBox();
+        menu.getChildren().addAll(goBack, goForward);
+        canvas.getChildren().add(menu);
+        // node.setTranslateY(50);
+        canvas.getChildren().add(node);
 
-	@Override
-	protected String getTitle() {
-		// TODO Auto-generated method stub
-		return "Simple Animation Example";
-	}
+    }
+
+    @Override
+    protected String getTitle() {
+        // TODO Auto-generated method stub
+        return "Simple Animation Example";
+    }
 
 
-	private void createAnimation() {
-		TranslateTransition tt;
-		TranslateTransition tt2;
-		TranslateTransition tt12;
-        ScaleTransition sc = ScaleTransitionBuilder.create().node(node).fromX(0).fromY(0).toX(1).toY(1).duration(Duration.seconds(5)).build();
-		tt = TranslateTransitionBuilder.create().fromX(0).fromY(50).toX(250)
-				.toY(50).duration(new Duration(1000)).node(node).build();
+    private void createAnimation() {
+        TranslateTransition tt;
+        TranslateTransition tt2;
+        TranslateTransition tt12;
+        //  ScaleTransition sc = ScaleTransitionBuilder.create().node(node).fromX(0).fromY(0).toX(1).toY(1).duration(Duration.seconds(5)).build();
+        tt = TranslateTransitionBuilder.create().fromX(0).fromY(50).toX(250)
+                .toY(50).duration(new Duration(1000)).node(node).build();
 
 
         tt12 = TranslateTransitionBuilder.create().fromX(250).fromY(50).toX(500)
-				.toY(50).duration(new Duration(1000)).node(node).build();
-		tt2 = TranslateTransitionBuilder.create().fromX(500).fromY(50).toX(500)
-				.toY(250).duration(new Duration(1000)).node(node).build();
+                .toY(50).duration(new Duration(1000)).node(node).build();
+        tt2 = TranslateTransitionBuilder.create().fromX(500).fromY(50).toX(500)
+                .toY(250).duration(new Duration(1000)).node(node).build();
+
+
+        Timeline timeline = new Timeline();
+        Duration duration = Duration.seconds(1);
+        timeline.getKeyFrames().addAll(
+                new KeyFrame(Duration.ZERO, new KeyValue(node.widthProperty(), 25, Interpolator.EASE_IN)),
+                new KeyFrame(duration, new KeyValue(node.widthProperty(), 25 * 2, Interpolator.EASE_IN))
+        );
+
+        ParallelTransition pt = ParallelTransitionBuilder.create().build();
+        pt.getChildren().addAll(timeline);
 
         SequentialTransition sq = SequentialTransitionBuilder.create().
-                children(tt,tt12).build();
+                children(tt, tt12, pt).build();
         sq.setOnFinished(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
@@ -111,7 +123,7 @@ public class SimpleAnimationExample extends AbstractExample {
             }
         });
 
-       TranslateTransition tt3 = TranslateTransitionBuilder.create().fromX(500).fromY(250).toX(0)
+        TranslateTransition tt3 = TranslateTransitionBuilder.create().fromX(500).fromY(250).toX(0)
                 .toY(250).duration(new Duration(1000)).node(node).build();
         tt3.setOnFinished(new EventHandler<ActionEvent>() {
             @Override
@@ -127,18 +139,18 @@ public class SimpleAnimationExample extends AbstractExample {
                 System.err.println("END 4 animation");
             }
         });
-        ac.getTransitions().add(sc);
-		ac.getTransitions().add(sq);
-		ac.getTransitions().add(new ParallelTransition(tt2));
-		ac.getTransitions().add(new ParallelTransition(tt3));
-		ac.getTransitions().add(new ParallelTransition(tt4));
+        //ac.getTransitions().add(sc);
+        ac.getTransitions().add(sq);
+        ac.getTransitions().add(new ParallelTransition(tt2));
+        ac.getTransitions().add(new ParallelTransition(tt3));
+        ac.getTransitions().add(new ParallelTransition(tt4));
 
-        if(!IS_STEPPING) {
+        if (!IS_STEPPING) {
             ac.playForward();
         }
-	}
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
